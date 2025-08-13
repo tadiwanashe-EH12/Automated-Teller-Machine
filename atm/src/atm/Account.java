9e9796946928de0f44054334cc50f8bcf8272b0a
tadiwanashe-EH12 <tadiwanshe1207@gmail.com>
2025-08-13 21:27:51 +0200

    added account constructor

--- atm/src/atm/Account.java
+++ atm/src/atm/Account.java
@@ -18,4 +18,21 @@ public class Account {
 // The transaction list for the account 
 	private ArrayList<Transaction> transactions;
 	
+	public Account(String name, User holder, Bank theBank) {
+		
+//		set the account name and holder
+		this.name = name;
+		this.holder = holder;
+		
+//		get new account UUID
+		this.uuid = theBank.getNewAccountUUID();
+		
+//		initialize transactions
+		this.transactions = new ArrayList<Transaction>();
+		
+//		add to holder and bank list
+		holder.addAccount(this);
+		theBank.addAccount(this);
+		
+	}
 }
--- atm/src/atm/Bank.java
+++ atm/src/atm/Bank.java
@@ -10,4 +10,7 @@ public class Bank {
 	public String getNewUserUUID() {
 		
 	}
+	public String getNewAccountUUID() {
+		
+	}
 }
