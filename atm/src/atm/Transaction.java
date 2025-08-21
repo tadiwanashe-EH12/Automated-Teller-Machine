3f7ee6ce7487edccff4f44f103f59a6d2890dbef
tadiwanashe-EH12 <tadiwanshe1207@gmail.com>
2025-08-21 10:10:08 +0200

    Set the memo

--- atm/src/atm/Transaction.java
+++ atm/src/atm/Transaction.java
@@ -13,15 +13,29 @@ public class Transaction {
 	
 //The account in which the transaction was performed 
 	private Account inAccount;
-	
+/*
+ * Create new Transaction
+ * @param amount  the amount transfered
+ * @param memo  the memo of the transaction
+ * @param inAccount  the account the transaction belongs to
+ * */
 	public Transaction(double amount, Account inAccount) {
 		this.amount = amount;
 		this.inAccount = inAccount;
 		this.timestamp = new Date();
 		this.memo = "";
 	}
+	/*
+	 * Create a new transaction
+	 * @param amount  the amount transacted
+	 * @param memo  the memo of the transaction
+	 * @param inAccount  the transaction the account belongs to
+	 * */
 	public Transaction(double amount, String memo, Account inAccount) {
 //		call the two Arg constructor first
 		this(amount, inAccount);
+//		set the memo
+		this.memo = memo;
+		
 	}
 }
